52cc4f33ffd5f4ad4fd07a13ae97510a0d16e787
josephsintum <sjumbam15@gmail.com>
2021-07-29 23:23:01 -0700

    adding pale color

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,5 @@
-import { createTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createTheme } from '@material-ui/core/styles'
+import { red } from '@material-ui/core/colors'
 
 // Create a theme instance.
 const theme = createTheme({
@@ -13,7 +13,9 @@ const theme = createTheme({
     error: {
       main: red.A400,
     },
+    // @ts-ignore
+    pale: '#D2FAFB',
   },
-});
+})
 
-export default theme;
+export default theme
