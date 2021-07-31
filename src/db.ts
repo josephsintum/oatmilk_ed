afa329bcf52597dcedbed5847ba3d3360abf8b00
josephsintum <sjumbam15@gmail.com>
2021-07-31 03:12:53 -0700

    db functions

---
+++ src/db.ts
@@ -0,0 +1,24 @@
+import { CloudantV1 } from '@ibm-cloud/cloudant'
+
+const service = CloudantV1.newInstance({})
+const projectsDB = 'projects'
+
+export function getAllProjects() {
+  return service
+    .postAllDocs({
+      db: projectsDB,
+      includeDocs: true,
+      limit: 10,
+    })
+    .then((response) => response.result.rows)
+}
+
+export function getProjectById(id: string) {
+  return service
+    .getDocument({
+      db: projectsDB,
+      docId: id,
+    })
+    .then((response) => response.result)
+    .catch((err) => console.log(err))
+}
